7a4b18557a17e3e3627643ccd41f1e7d8ee90f8e
Ankur Patel <ankur.patel07@outlook.com>
2024-01-12 08:52:00 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -24,58 +24,7 @@ const Task = mongoose.model('Task', {
     content: String,
   });
 
-app.use('/api',loginroutes)
-
-app.get('/api/tasks', async (req, res) => {
-    try {
-      const tasks = await Task.find();
-      res.json({ tasks });
-    } catch (error) {
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
-
-  app.post('/api/tasks', async (req, res) => {
-    const { content } = req.body;
-  
-    try {
-      const task = new Task({ content });
-      await task.save();
-      res.json({ task });
-    } catch (error) {
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
-
-app.put('/api/tasks/:id', async (req, res) => {
-    const { id } = req.params;
-    const { content } = req.body;
-  
-    try {
-      const task = await Task.findByIdAndUpdate(id, { content }, { new: true });
-      res.json({ task });
-    } catch (error) {
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
-
-
-app.delete('/api/tasks/:id', async (req, res) => {
-    const { id } = req.params;
-  
-    try {
-      await Task.findByIdAndDelete(id);
-      res.json({ success: true });
-    } catch (error) {
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
-  
-
-
-
-
-
+app.use('/api',loginroutes);
 
 const PORT=process.env.PORT||5055;
 app.listen(PORT,()=>{
